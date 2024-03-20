f3410d29a12b3c17e85cdfd383ab4eb63cd97d3c
Jason Hankins <tul53850@temple.edu>
2024-03-20 15:38:11 -0400

    Refactored functionality to fragment

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -2,21 +2,41 @@ package edu.temple.fragmentrefactor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val changeButton = findViewById<Button>(R.id.changeButton)
+        /*supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, MyFragment())
+            .commit()*/
+    }
+}
+
+class MyFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment, container, false)
+
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
 
         changeButton.setOnClickListener {
-            val name = nameEditText.text
+            val name = nameEditText.text.toString()
 
             displayTextView.text = if (name.isNotBlank()) {
                 "Hello, $name!"
@@ -24,5 +44,7 @@ class MainActivity : AppCompatActivity() {
                 "Please enter your name"
             }
         }
+
+        return view
     }
 }
